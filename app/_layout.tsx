6ae7158d83255cf5943415973c0104264d88c39f
chamila <balasuriye@gmail.com>
2026-02-09 17:58:41 +0530

    Added safearea view

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,17 @@
 import { Stack } from 'expo-router';
 import 'react-native-reanimated';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
 
   return (
-
-    <Stack>
-      <Stack.Screen name="home-screen" options={{ headerShown: false }} />
-      <Stack.Screen name="details-screen" options={{ headerShown: false }} />
-      <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-    </Stack>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Stack>
+        <Stack.Screen name="home-screen" options={{ headerShown: false }} />
+        <Stack.Screen name="details-screen" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      </Stack>
+    </SafeAreaView>
 
   );
 }
--- app/details-screen.tsx
+++ app/details-screen.tsx
@@ -1,10 +1,12 @@
 import { Text, View } from "react-native";
-
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function HomeScreen() {
 
     return (
-        <View>
-            <Text>Home Screen</Text>
-        </View>
+        <SafeAreaView>
+            <View>
+                <Text>Home Screen</Text>
+            </View>
+        </SafeAreaView>
     )
 }
